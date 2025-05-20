8adf882cef481b7b1d8ba4c1f186c8d751865359
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-05-20 14:25:03 +0100

    some refactoring of Floaters

--- src/scripts/Floater.js
+++ src/scripts/Floater.js
@@ -5,37 +5,47 @@ class Floater {
     if (!container || !(container instanceof HTMLElement)) {
       throw new Error('A valid container element must be provided.');
     }
-
+    this.setRevealPosition(revealX, revealY);
+    this.createPersonality();
     this.createFloaterDiv(layoutNumber);
 
-    this.container = container;
-    // this.container.style.zIndex = World.DEPTH * -1;// This is what was causing the lack of pointer events reaching the floaters
+    this.container = container; //Do not give container a z-index!
     this.container.style.position = 'relative';
-    // this.containerRect = this.container.getBoundingClientRect(); // Get the bounding rectangle of the container
+    this.container.appendChild(this.element);
     this.setPosition(
       World.BIRTH_POSITION.x,
       World.BIRTH_POSITION.y,
       World.BIRTH_POSITION.z
-    ); // Set initial position
-    this.revealX = revealX; // Set the reveal position
-    this.revealY = revealY; // Set the reveal position
+    );
+    this.isFloating = true;
 
-    //set up the floater's 'personality'
+    const { x, y, z } = this.createRandomPosition();
+    this.moveTo(x, y, z, this.myDuration); // move to initial position
+  }
+
+  createPersonality() {
     this.speed = Math.random() * World.MAX_SPEED + World.MIN_SPEED;
     this.myDuration = World.DURATION * this.speed;
     this.easingStyle =
       World.POSSIBLE_EASING_STYLES[
         Math.floor(Math.random() * World.POSSIBLE_EASING_STYLES.length)
       ];
-    this.isFloating = true;
-    this.container.appendChild(this.element);
+  }
 
-    //This was for calculating their starting positions when using relative positioning - now moved on to absolute positioning with a display position passed through as revealX and revealY
-    // this.startX = this.element.getBoundingClientRect().x;
-    // this.startY = this.element.getBoundingClientRect().y;
-    // console.log(this.startX, this.startY);
-    const { x, y, z } = this.createRandomPosition();
-    this.moveTo(x, y, z, this.myDuration); // move to initial position
+  setRevealPosition(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      throw new Error('Reveal position coordinates must be numbers.');
+    }
+    this.revealX = x;
+    this.revealY = y;
+  }
+
+  setDimensions(width, height) {
+    if (typeof width !== 'number' || typeof height !== 'number') {
+      throw new Error('Width and height must be numbers.');
+    }
+    this.element.style.width = `${width}px`;
+    this.element.style.height = `${height}px`;
   }
 
   createFloaterDiv(layoutNumber) {
@@ -55,16 +65,19 @@ class Floater {
   }
 
   float() {
-    let counter = 0;
+    this.isFloating = true;
+    // this was when I was playing with the divs being self aware as it were
+    this.element.setAttribute('data-floating', this.isFloating);
+    // remove the resize listener as it's not needed whilst we're floating
     if (this.resizeListener) {
       window.removeEventListener('resize', this.resizeListener);
       this.hasHeardResize = false;
     }
-    this.contentHolder.style.visibility = 'hidden'; // Hide the content holder initially
-    // const myDuration = World.DURATION * this.speed;
+    // just unneccesarily processor intensive to show the contents whilst floating (and the reason for the name 'reveal')
+    this.contentHolder.style.visibility = 'hidden';
+    // depending on the individual personality this creates the actual movement cyclically
     this.floatInterval = setInterval(() => {
       const { x, y, z } = this.createRandomPosition();
-      counter++;
       this.moveTo(x, y, z, this.myDuration); // Move to new position
       //   console.log(`z-index: ${z}`); // Log the z-index for debugging
     }, this.myDuration);
@@ -81,51 +94,61 @@ class Floater {
     return { x, y, z };
   }
 
+  // This does the grunt work of actually setting the style properties including the transform (used for scaling and rotation)
   setPosition(x, y, z = 1) {
-    if (
-      typeof x !== 'number' ||
-      typeof y !== 'number' ||
-      (z !== undefined && typeof z !== 'number')
-    ) {
-      throw new Error('Position coordinates must be numbers.');
+    typeof x !== 'number' && (x = parseInt(x));
+    typeof y !== 'number' && (y = parseInt(y));
+    typeof z !== 'number' && (z = parseInt(z));
+    if (isNaN(x) || isNaN(y) || isNaN(z)) {
+      console.error('Invalid position values for setPosition:', x, y, z);
+      return;
     }
     this.element.style.left = `${x}px`;
     this.element.style.top = `${y}px`;
     this.element.style.zIndex = z;
-    this.setZBasedScale(z);
-    //this must be called AFTER the setZBasedScale function to avoid overwriting the transform property
-    this.rotateTowardsTarget(x, y);
+    let transformStr;
+    transformStr = this.setZBasedScale(z);
+    transformStr += ' ';
+    transformStr += this.rotateTowardsTarget(x, y);
+    this.element.style.transform = transformStr;
   }
 
-  // Due to the += operator this needs to be called AFTER the setZBasedScale function to avoid overwriting the transform property
+  // change these to return their transform strings instead
   rotateTowardsTarget(targetX, targetY) {
-    if (typeof targetX !== 'number' || typeof targetY !== 'number') {
-      throw new Error('Target coordinates must be numbers.');
+    typeof targetX !== 'number' && (targetX = parseInt(targetX));
+    typeof targetY !== 'number' && (targetY = parseInt(targetY));
+    if (isNaN(targetX) || isNaN(targetY)) {
+      console.error(
+        'Invalid target values for rotateTowardsTarget:',
+        targetX,
+        targetY
+      );
+      return;
     }
     const dx = targetX - parseFloat(this.element.style.left);
     const dy = targetY - parseFloat(this.element.style.top);
     const angle = Math.atan2(dy, dx) * (180 / Math.PI); // Convert radians to degrees
-    this.element.style.transform += `rotate(${angle}deg)`;
+    return `rotate(${angle}deg)`; // Return the rotation transform string
   }
 
   setZBasedScale(z) {
     if (typeof z !== 'number') {
-      throw new Error('Z coordinate must be a number.');
+      z = parseInt(z);
     }
-    this.element.style.transform = `scale(${
-      (95 / World.DEPTH) * (World.DEPTH / Math.abs(z)) + 5
-    }%)`;
+    // just fiddled around with so that they don't become too small
+    return `scale(${(95 / World.DEPTH) * (World.DEPTH / Math.abs(z)) + 5}%)`;
   }
 
+  // essentially set's the transition property before calling setPosition()
   moveTo(x, y, z, duration = 500) {
-    if (
-      typeof x !== 'number' ||
-      typeof y !== 'number' ||
-      typeof z !== 'number'
-    ) {
-      throw new Error('Position coordinates must be numbers.');
+    typeof x !== 'number' && (x = parseInt(x));
+    typeof y !== 'number' && (y = parseInt(y));
+    typeof z !== 'number' && (z = parseInt(z));
+    if (isNaN(x) || isNaN(y) || isNaN(z)) {
+      console.error('Invalid position values for moveTo:', x, y, z);
+      return;
     }
-    this.element.style.transition = `all ${duration}ms ${this.easingStyle}`; // Smooth transition
+    this.element.style.transition = `all ${duration}ms ${this.easingStyle}`;
     this.setPosition(x, y, z);
   }
 
@@ -134,16 +157,16 @@ class Floater {
     clearInterval(this.floatInterval);
     // z-index of 1 passed in as we don't want any scaling
     this.moveTo(this.revealX, this.revealY, 1, World.DURATION);
-    // now that we are going to be making up a page layout we want to debounce any window resizes so we can look after the container height
+    // add a resize listener so we can look after the container height
     this.resizeListener = window.addEventListener('resize', () => {
       if (!this.hasHeardResize) {
         this.hasHeardResize = true;
-        //add a little 'debounce' (I think it's called) so that we don't react to 100s of resize events that are triggered by the browser being resized
+        //add a little 'debounce' so that we don't react to 100s of resize events that are triggered by the browser being resized
         setTimeout(() => {
           console.log('Resizing container...');
           this.resizeContainerRect();
           this.hasHeardResize = false;
-        }, 1000); // Delay of 1 sec to allow for resizing
+        }, 1000);
       }
     });
     // This is what we want to happen at the end of the reveal hence it's a timeout with the same duration as the moveTo function is using (I've made it fractionally longer so that the call to resizeContainerRect works as hoped)
@@ -158,12 +181,15 @@ class Floater {
 
   // Set the container height to fit content so it will scroll when the floaters overflow the height of the container
   resizeContainerRect() {
+    if (this.isFloating) {
+      return;
+    }
     const containerRect = this.container.getBoundingClientRect();
     const elementRect = this.element.getBoundingClientRect();
-    if (parseFloat(containerRect.bottom) < elementRect.bottom) {
-      this.container.style.height =
-        elementRect.bottom - containerRect.top + World.CONTENT_PADDING + 'px';
-    }
+    // if (parseFloat(containerRect.bottom) < elementRect.bottom) {
+    this.container.style.height =
+      elementRect.bottom - containerRect.top + World.CONTENT_PADDING + 'px';
+    // }
   }
 }
 
--- src/scripts/FloaterInner.js
+++ src/scripts/FloaterInner.js
@@ -5,31 +5,18 @@ class FloaterInner extends Floater {
   constructor(src, ...rest) {
     super(...rest);
     this.src = Page_Test.content[src].html;
-    console.log(this.src);
-    //Add an element to hold the content so it can have it's visibilty toggled in float() and reveal()
+    // console.log(this.src);
+    //Add an element to hold the content so it can have it's visibilty toggled in super.float() and super.reveal()
     this.contentHolder = document.createElement('div');
     this.contentHolder.className = 'content-viewport';
-    const content = document.createElement('div');
-    content.className = 'content-holder';
-    content.innerHTML = this.src;
-    this.contentHolder.appendChild(content);
-    // this.contentHolder = document.createElement('div');
-    // this.iframer = document.createElement('iframe');
-    // this.contentHolder.className = 'content-holder';
-    // this.iframer.setAttribute('sandbox', 'allow-scripts allow-same-origin');
-    // this.contentHolder.setAttribute(
-    //   'sandbox',
-    //   'allow-scripts allow-same-origin'
-    // );
-    // this.contentHolder.appendChild(this.iframer);
-    // this.contentHolder.setAttribute('src', this.iframeUrl);
     this.element.appendChild(this.contentHolder);
   }
 
   reveal() {
-    // this.iframer.setAttribute('src', this.iframeUrl);
-    // this.contentHolder.setAttribute('src', this.iframeUrl);
-    // this.iframer.setAttribute('scrolling', 'yes');
+    const content = document.createElement('div');
+    content.className = 'content-holder';
+    content.innerHTML = this.src;
+    this.contentHolder.appendChild(content);
     super.reveal();
   }
 }
