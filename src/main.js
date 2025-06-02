e6d75b1c53bcf7ca3a1180a93a972bc35495c228
Giles Webberley <74770294+gileswebberley@users.noreply.github.com>
2025-06-02 20:21:49 +0100

    simply adding info in footer and icon styling

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import './style.css';
-import Floater from './scripts/Floater.js';
-import FloaterIframe from './scripts/FloaterIframe.js';
-import FloaterInner from './scripts/FloaterInner.js';
 import SiteManager from './scripts/SiteManager.js';
 import Page_Test from './contents/TestContent.js';
 
 //we'll remove the loading message when this loads...
 document.querySelector('#loader').remove();
-document.querySelector('#email-address').textContent =
-  'gileswebberley@gmail.com';
+//adding my email address like this to try to avoid being the victim of spam...
+const noHarvestSubject = encodeURI('Message from portfolio site');
+document.querySelector('#email-address').innerHTML = ` <a
+              href='mailto:gileswebberley@gmail.com?subject=${noHarvestSubject}'
+              target="_blank"
+              rel="noreferrer"
+            >
+            <i class="fa-regular fa-envelope"></i></a> gileswebberley@gmail.com`;
 
 const container = document.querySelector('#container');
 const navElement = document.querySelector('#main-nav');
